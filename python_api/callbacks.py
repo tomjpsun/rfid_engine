b7dc0695f02cba336e4bcae9400c227b5a404111
Ching-Ping Sun <tomjpsun@gmail.com>
2021-01-08 11:28:25 +0800

    [py] provide complete python interfaces

--- python_api/callbacks.py
+++ python_api/callbacks.py
@@ -37,8 +37,38 @@ def antenna_callback(antenna_data, size):
     print(antenna_str)
 
 
+# coordinate_callback(coordinate_data, size):
+#
+#     coordinate_data is a ctypes.LP_c_char object,
+#         use array operator[] to extract its content to bytearray, and
+#         decode bytearray to python string
+#     size is the bytes count in antenna_data
+
+def coordinate_callback(coordinate_data, size):
+    print("get coordinate_data({}) = ".format(size))
+    coordinate_str = (coordinate_data[:size]).decode("utf-8")
+    print(coordinate_str)
+
+
+# statistics_callback(statistics_data, size):
+#
+#     statistics_data is a ctypes.LP_c_char object,
+#         use array operator[] to extract its content to bytearray, and
+#         decode bytearray to python string
+#     size is the bytes count in antenna_data
+
+def statistics_callback(statistics_data, size):
+    print("get statistics_data({}) = ".format(size))
+    statistics_str = (statistics_data[:size]).decode("utf-8")
+    print(statistics_str)
+
+
+# make python function to c type function
 antenna_cb = CALLBACK_T(antenna_callback)
+coordinate_cb = CALLBACK_T(coordinate_callback)
+statistics_cb = CALLBACK_T(statistics_callback)
 
 f = Foo()
-f.bar()
 f.get_antenna_data(antenna_cb)
+f.get_coordinate(coordinate_cb)
+f.get_statistics(statistics_cb)
