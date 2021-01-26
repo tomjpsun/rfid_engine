d4506b753d8cd0251db1b214df5cb8bfae1b8165
Ching-Ping Sun <tomjpsun@gmail.com>
2021-01-26 17:24:57 +0800

    [PythonAPI] remove test class

--- python_api/test.py
+++ python_api/test.py
@@ -1,30 +1,5 @@
 from ctypes import *
 
-class Foo():
-    def __init__(self):
-        print('__init__')
-        self.obj = lib.Foo_new()
-        print('self.obj={}'.format(self.obj))
-
-    def bar(self):
-        lib.Foo_bar(self.obj)
-
-    def get_antenna_data(self):
-        lib.Foo_get_antenna_data(self.obj)
-
-    def get_coordinate(self):
-        lib.Foo_get_coordinate(self.obj)
-
-    def get_statistics(self):
-        lib.Foo_get_statistics(self.obj)
-
-lib = cdll.LoadLibrary('/usr/local/lib/librfidmgr.so')
-print('lib = {}'.format(lib))
-f = Foo()
-f.bar()
-f.get_antenna_data()
-f.get_coordinate()
-f.get_statistics()
 # test callback from libc qsort(),
 # the last param of qsort() is a comparison function,
 # here we provide a python function as the comparison function,
