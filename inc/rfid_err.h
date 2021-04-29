d9e53669ee59771802d84cec89a0a55570202f4e
Ching-Ping Sun <tomjpsun@gmail.com>
2021-04-29 17:59:55 +0800

    Define API error codes

---
+++ inc/rfid_err.h
@@ -0,0 +1,13 @@
+#ifndef __RFID_ERR_H__
+#defien __RFID_ERR_H__
+
+#define RFID_OK                           1
+#define RFID_ERR_OTHER                    2
+#define RFID_ERR_MEMORY_OVERRUN           3
+#define RFID_ERR_MEMORY_LOCKED            4
+#define RFID_ERR_INSUFFICIENT_POWER       5
+#define RFID_ERR_NONSPECIFIC              6
+#define RFID_ERR_WORDS_WRITE_ERR_Z      700
+#define RFID_ERR_WORDS_WRITE_ERR_3Z     800
+
+#endif
