8b78a645fd6b34db43d1d7d80dc4d6c38110c44a
Ching-Ping Sun <tomjpsun@gmail.com>
2021-05-13 15:35:06 +0800

    [Py_API] wrap calls to the RfidInterface is successful

--- python_api/callbacks.py
+++ python_api/callbacks.py
@@ -8,14 +8,10 @@ CALLBACK_T = CFUNCTYPE(None, POINTER(c_char), c_size_t)
 class Foo():
     def __init__(self, ip, port):
         print('__init__')
-        self.obj = lib.Foo_new(, 1001)
+        ip_bytes = ip.encode('utf-8')
+        self.obj = lib.Foo_new(c_char_p(ip_bytes), c_int(port))
         print('self.obj={}'.format(self.obj))
 
-    def bar(self):
-        lib.Foo_bar(self.obj)
-
-    def get_antenna_data(self, cb_func):
-        lib.Foo_get_antenna_data(self.obj, cb_func)
 
     def get_coordinate(self, cb_func):
         lib.Foo_get_coordinate(self.obj, cb_func)
@@ -23,8 +19,7 @@ class Foo():
     def get_statistics(self, cb_func):
         lib.Foo_get_statistics(self.obj, cb_func)
 
-    def InventoryEPC(self, cb_func):
-        lib.Foo_InventoryEPC(
+
 
 # antenna_callback(antenna_data, size):
 #
@@ -76,8 +71,4 @@ coordinate_cb = CALLBACK_T(coordinate_callback)
 statistics_cb = CALLBACK_T(statistics_callback)
 inventoryEPC_cb = CALLBACK_T(inventoryEPC_callback)
 
-f = Foo()
-f.get_antenna_data(antenna_cb)
-f.get_coordinate(coordinate_cb)
-f.get_statistics(statistics_cb)
-f.InventoryEPC(
+f = Foo("192.168.88.91", 1001)
