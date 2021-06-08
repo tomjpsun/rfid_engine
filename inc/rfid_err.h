6c7b21cf5f7bda761dcc212234726aebfd1835f6
Ching-Ping Sun <tomjpsun@gmail.com>
2021-06-08 10:01:57 +0800

    [CPP_IF] Define Error code Category

--- inc/rfid_err.h
+++ inc/rfid_err.h
@@ -1,31 +1,32 @@
 #ifndef __RFID_ERR_H__
 #define __RFID_ERR_H__
 
+#define RFID_LIB_SCOPE                    0x00001000
 
-#define RFID_OK                           0x1
-#define RFID_ERR_OTHER                    0x2
-#define RFID_ERR_MEMORY_OVERRUN           0x3
-#define RFID_ERR_MEMORY_LOCKED            0x4
-#define RFID_ERR_INSUFFICIENT_POWER       0x5
-#define RFID_ERR_NONSPECIFIC              0x6
-#define RFID_ERR_NO_RESPONSE              0x7
-#define RFID_ERR_PARSE                    0x8
-#define RFID_ERR_REGULATION               0x9
-#define RFID_ERR_ECHO_RESULT              0xa
-#define RFID_ERR_INPUT                    0xb
-#define RFID_ERR_SEND                     0xc
-#define RFID_ERR_THREAD_CREATE            0xd
-#define RFID_ERR_INVALID_HANDLE           0xe
-#define RFID_ERR_BUFFER_OVERFLOW          0xf
-#define RFID_ERR_FILE_NOT_EXIST          0x10
-#define RFID_ERR_WORDS_WRITE_ERR_Z      0x100
-#define RFID_ERR_WORDS_WRITE_ERR_3Z     0x200
+#define RFID_OK                           (RFID_LIB_SCOPE | 0x01 )
+#define RFID_ERR_OTHER                    (RFID_LIB_SCOPE | 0x02 )
+#define RFID_ERR_MEMORY_OVERRUN           (RFID_LIB_SCOPE | 0x03 )
+#define RFID_ERR_MEMORY_LOCKED            (RFID_LIB_SCOPE | 0x04 )
+#define RFID_ERR_INSUFFICIENT_POWER       (RFID_LIB_SCOPE | 0x05 )
+#define RFID_ERR_NONSPECIFIC              (RFID_LIB_SCOPE | 0x06 )
+#define RFID_ERR_NO_RESPONSE              (RFID_LIB_SCOPE | 0x07 )
+#define RFID_ERR_PARSE                    (RFID_LIB_SCOPE | 0x08 )
+#define RFID_ERR_REGULATION               (RFID_LIB_SCOPE | 0x09 )
+#define RFID_ERR_ECHO_RESULT              (RFID_LIB_SCOPE | 0x0a )
+#define RFID_ERR_INPUT                    (RFID_LIB_SCOPE | 0x0b )
+#define RFID_ERR_SEND                     (RFID_LIB_SCOPE | 0x0c )
+#define RFID_ERR_THREAD_CREATE            (RFID_LIB_SCOPE | 0x0d )
+#define RFID_ERR_INVALID_HANDLE           (RFID_LIB_SCOPE | 0x0e )
+#define RFID_ERR_BUFFER_OVERFLOW          (RFID_LIB_SCOPE | 0x0f )
+#define RFID_ERR_FILE_NOT_EXIST           (RFID_LIB_SCOPE | 0x10 )
+#define RFID_ERR_WORDS_WRITE_ERR_Z        (RFID_LIB_SCOPE | 0x100)
+#define RFID_ERR_WORDS_WRITE_ERR_3Z       (RFID_LIB_SCOPE | 0x200)
 
 
 
 inline bool IS_OK (int n) { return n == RFID_OK; };
-inline bool IS_ERRZ  (int n) { return (n & 0x00000F00) == RFID_ERR_WORDS_WRITE_ERR_Z; }
-inline bool IS_ERR3Z (int n) { return (n & 0x00000F00) == RFID_ERR_WORDS_WRITE_ERR_3Z; }
+inline bool IS_ERRZ  (int n) { return (n & 0xFFFFFF00) == RFID_ERR_WORDS_WRITE_ERR_Z; }
+inline bool IS_ERR3Z (int n) { return (n & 0xFFFFFF00) == RFID_ERR_WORDS_WRITE_ERR_3Z; }
 inline int  ERRZ_VAL (int n) { return n & 0x000000FF; }
 inline int  ERR3Z_VAL (int n) { return n & 0x000000FF; }
 
