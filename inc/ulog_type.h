8b6c8bb7dd59bd763c9dffcda8dffe301673c371
Ching-Ping Sun <tomjpsun@gmail.com>
2022-03-23 16:55:55 +0800

    add Ulog fields

--- inc/ulog_type.h
+++ inc/ulog_type.h
@@ -10,12 +10,17 @@ using json = nlohmann::json;
 namespace ulog_namespace {
 	class Ulog {
 	public:
-		Ulog(string logger_id, int log_level, string msg):
-			logger_id(logger_id), log_level(log_level), msg(msg)
+		Ulog(string logger_id, int log_level, string msg,
+		     string app_name = "no_app",
+		     int app_flags = 0 ):
+			logger_id(logger_id), log_level(log_level), msg(msg),
+			app_name(app_name), app_flags(app_flags)
 			{ }
 		string logger_id;
 		int log_level;
 		string msg;
+		string app_name;
+		int app_flags;
 	};
 	void to_json(json& j, const Ulog& log);
 	void from_json(const json& j, Ulog& log);
